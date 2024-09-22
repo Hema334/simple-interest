fbd791284049cfc061a925de130d86f47dc3771a
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-22 14:40:33 +0530

    Add files via upload

---
+++ simple intrest.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+// C program to find the simple interest
+#include <stdio.h>
+
+int main() {
+  
+    // Input values
+    float P = 2, R = 4, T = 7;
+
+    // Calculate simple interest
+    float SI = (P * T * R) / 100;
+
+    // Print Simple Interest
+    printf("Simple Interest = %f\n", SI);
+
+    return 0;
+}
